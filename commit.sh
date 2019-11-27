99d30e8876c85fdd4a5d1a46812707a50552ac7a
Webdozen <microlion116@gmail.com>
2019-11-27 10:21:34 +0300

    Initial commit

---
+++ commit.sh
@@ -0,0 +1,12 @@
+Y=2019
+M=11
+D=27
+i=21
+comment=" Initial commit"
+
+export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
+export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
+# git add commit.md -f
+git add .
+git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
+git push
