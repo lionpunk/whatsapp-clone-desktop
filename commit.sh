369856ada059b3b19050de18ef4b82cae06f74e4
Webdozen <microlion116@gmail.com>
2019-11-28 10:22:34 +0300

    Update README.md

--- commit.sh
+++ commit.sh
@@ -2,8 +2,7 @@ Y=2019
 M=11
 D=28
 i=22
-comment=" Add Debian package build 
-script"
+comment=" Update README.md"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
